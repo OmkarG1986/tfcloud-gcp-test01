a75affe7589ddbb25549685e3b4b1603f0111b37
Omkar Sonawane <omkar.o.sonawane@henkel.com>
2022-11-15 19:55:07 +0100

    provider.tf added

---
+++ terraform/simple_bucket/provider.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  impersonate_service_account = "hmcp-f-tst-wifom-sa-proj@hmcp-tst-wifom-de-prj-xzo.iam.gserviceaccount.com"
+  project                     = "hmcp-tst-wifom-de-prj-xzo"
+}
